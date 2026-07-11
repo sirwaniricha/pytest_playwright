c37d7d051ffe896fe3feee986ce1495303d2e07f
Richa Sirwani <sirwani.richa@gmail.com>
2026-07-12 00:00:46 +0530

    added provision to validate negative testcase

--- saucedemo_automation/utilities/login.py
+++ saucedemo_automation/utilities/login.py
@@ -2,23 +2,25 @@ import requests
 from playwright import sync_api
 
 class Login:
-    def __init__(self,data):
-        self.user_name = data['username']
-        self.password = data['password']
-        self.env = data.get('env')
 
-    def do_login(self, page: sync_api.Page):
 
+    def do_login(self, page: sync_api.Page,data):
         page.goto("https://www.saucedemo.com")
-        page.locator("//*[@data-test='user-name']").fill(self.user_name)
-        page.locator("//*[@data-test='password']").fill(self.password)
+        page.locator("//*[@data-test='user-name']").fill(data["username"])
+        page.locator("//*[@data-test='password']").fill(data["password"])
         page.locator('//*[@data-test="login-button"]').click()
-        self.validate_login(page)
+        if "expected_error_message" not in data:
+            self.validate_login(page)
+        else:
+            self.validate_negative_login(page, data["expected_error_message"])
 
     def validate_login(self, page: sync_api.Page):
         try:
             sync_api.expect(page).to_have_title("Swag Labs")
         except:
             print("Login failed")
+        else:
+            print("Login successful")
 
-
+    def validate_negative_login(self, page: sync_api.Page, message: str):
+        print("validating login : negative case")
